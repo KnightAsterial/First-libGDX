21eb385a347cb8b7672537eae3cc41435e62414e
Ryan@Home <ryanzhao@live.com>
2016-12-24 12:50:45 -0500

    Intial Commit

---
+++ html/src/com/knightasterial/testgame/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.knightasterial.testgame.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.knightasterial.testgame.MyTestGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new MyTestGame();
+        }
+}
